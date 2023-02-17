c8409deb9eb8986d03fa29d844519e5439b1b19c
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-02-17 14:56:26 -0400

    fixing task 5

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 """Module base_model"""
 import uuid
 from datetime import datetime
-from models.__init__ import storage
+import models
 
 class BaseModel:
     """BaseModel class"""
@@ -19,7 +19,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
         """str method"""
@@ -28,7 +28,7 @@ class BaseModel:
     def save(self):
         """save method"""
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """to_dict method"""
