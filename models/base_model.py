c506bbba0cb56f121b5c6f98960a8b165e7ca341
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-02-17 10:00:03 -0400

    updating init method

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,18 @@ from datetime import datetime
 class BaseModel:
     """BaseModel class"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Constructor"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for k, v in kwargs.items():
+                if k in ['created_at', 'updated_at']:
+                    self.__dict__[k] = datetime.strptime(v, '%Y-%m-%dT%H:%M:%S.%f')
+                elif k != '__class__':
+                    self.__dict__[k] = v
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """str method"""
