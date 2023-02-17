b35f9b2608b70340224eca3bd6ae0caaf2c40477
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-02-17 14:01:26 -0400

    uploading changes for task 5

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 """Module base_model"""
 import uuid
 from datetime import datetime
-
+from models.__init__ import storage
 
 class BaseModel:
     """BaseModel class"""
@@ -19,6 +19,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """str method"""
@@ -27,6 +28,7 @@ class BaseModel:
     def save(self):
         """save method"""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """to_dict method"""
---
+++ models/engine/file_storage.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Module file_storage"""
+import json
+from models.base_model import BaseModel
+
+
+class FileStorage():
+    """FileStorage class"""
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """Returns the dictionary __objects"""
+        return self.__objects
+
+    def new(self, obj):
+        """Sets in __objects the obj"""
+        self.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
+
+    def save(self):
+        """Serializes __objects to the JSON file"""
+        with open(self.__file_path, "w", encoding='utf-8') as f:
+            json.dump({k: v.to_dict() for k, v in self.__objects.items()}, f)
+
+    def reload(self):
+        """Deserializes the JSON file to __objects"""
+        try:
+            with open(self.__file_path, "r", encoding='utf-8') as f:
+                self.__objects = {k: BaseModel(**v) for k, v in json.load(f).items()}
+        except Exception:
+            pass
