06b7fb80d295247393a92f4a0db94c8da3813963
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-02-20 16:22:13 -0400

    uploading just the classes without any methods

--- models/city.py
+++ models/city.py
@@ -1,33 +1,9 @@
 #!/usr/bin/python3
 """Module city"""
-from datetime import datetime
 from models.base_model import BaseModel
-import models
 
 
 class City(BaseModel):
     """Class for city"""
     state_id = ""
     name = ""
-
-    def __init__(self, *args, **kwargs):
-        """Constructor"""
-        super().__init__(*args, **kwargs)
-        models.storage.new(self)
-
-    def __str__(self):
-        """str method"""
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
-
-    def save(self):
-        """save method"""
-        self.updated_at = datetime.now()
-        models.storage.save()
-
-    def to_dict(self):
-        """to_dict method"""
-        dic_copy = self.__dict__.copy()
-        dic_copy['__class__'] = self.__class__.__name__
-        dic_copy['created_at'] = self.created_at.isoformat()
-        dic_copy['updated_at'] = self.updated_at.isoformat()
-        return dic_copy
