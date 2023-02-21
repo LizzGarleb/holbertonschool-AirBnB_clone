64968a9534e7403e0a20b06b595e062a4d07719d
LizzGarleb <redscorpion3108@gmail.com>
2023-02-21 11:01:01 -0400

    Task 10 complete

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,11 +3,17 @@
 import json
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
 
 class FileStorage():
     """FileStorage class"""
     __file_path = "file.json"
     __objects = {}
+    classes = {}
 
     def all(self):
         """Returns the dictionary __objects"""
