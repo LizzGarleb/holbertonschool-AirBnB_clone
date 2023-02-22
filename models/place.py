ecf4db1c2c0afd77d2593fe6d337d40adc7f7e30
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-02-22 17:01:52 -0400

    uploading tests for Amenity and Place class, fixing typo in Place class

--- models/place.py
+++ models/place.py
@@ -2,7 +2,9 @@
 """
 Module place
 """
+from datetime import datetime
 from models.base_model import BaseModel
+import models
 
 
 class Place(BaseModel):
@@ -19,4 +21,26 @@ class Place(BaseModel):
     price_by_night = 0
     latitude = 0.0
     longitude = 0.0
-    amenity = []
+    amenity_ids = []
+
+    def __init__(self, *args, **kwargs):
+        """Constructor\n"""
+        super().__init__(*args, **kwargs)
+        models.storage.new(self)
+
+    def __str__(self):
+        """str method\n"""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """save method\n"""
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        """to_dict method\n"""
+        dic_copy = self.__dict__.copy()
+        dic_copy['__class__'] = self.__class__.__name__
+        dic_copy['created_at'] = self.created_at.isoformat()
+        dic_copy['updated_at'] = self.updated_at.isoformat()
+        return dic_copy
