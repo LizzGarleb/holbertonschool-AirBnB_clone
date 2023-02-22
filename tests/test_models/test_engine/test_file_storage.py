3e5313ef3b4e7937cec991df425caf55cb3c76a6
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-02-22 14:20:18 -0400

    fixing every test method

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,35 +1,62 @@
 import unittest
 import json
+import os
 from models.base_model import BaseModel
+from models.city import City
 from models.engine.file_storage import FileStorage
+from models import storage
+from models.user import User
 
 
 class TestFileStorage(unittest.TestCase):
     """ Testing File Storae functionality """
 
+    def setUp(self):
+        """Set up"""
+        self.file_path = 'file.json'
+        self.obj1 = BaseModel()
+        self.obj2 = User()
+        self.obj1.save()
+        self.obj2.save()
+
+    def tearDown(self):
+        """Tear down"""
+        os.remove(self.file_path)
+
     def test_all(self):
         """Test all"""
-        fs1 = FileStorage()
-        fs2 = FileStorage()
-        self.assertEqual(fs1.all(), fs2.all())
+        objs = storage.all()
+        self.assertEqual(len(objs), 2)
+        self.assertIn(f"{type(self.obj1).__name__}.{self.obj1.id}", objs)
+        self.assertIn(f"{type(self.obj2).__name__}.{self.obj2.id}", objs)
 
     def test_new(self):
         """Test new"""
-        fs1 = FileStorage()
-        bm1 = BaseModel()
-        fs1.new(bm1)
-        self.assertEqual(fs1.all(), {f'BaseModel.{bm1.id}': bm1})
+        obj3 = City()
+        storage.new(obj3)
+        objs = storage.all()
+        self.assertEqual(len(objs), 5)
+        self.assertIn(f"{type(obj3).__name__}.{obj3.id}", objs)
 
     def test_save(self):
         """Test save"""
-        fs1 = FileStorage()
-        bm1 = BaseModel()
-        fs1.new(bm1)
-        fs1.save()
-        with open('file.json', 'r') as f:
+        with open(self.file_path, 'r') as f:
             data = json.load(f)
-            self.assertEqual(len(data), 2)
-            self.assertIn(f'BaseModel.{bm1.id}', data)
+            self.assertEqual(len(data), 8)
+            self.assertIn(f"{type(self.obj1).__name__}.{self.obj1.id}", data)
+            self.assertIn(f"{type(self.obj2).__name__}.{self.obj2.id}", data)
+    
+    def test_reload(self):
+        """Test reload"""
+        with open(self.file_path, 'r') as f:
+            data = json.load(f)
+            del data[f"{type(self.obj1).__name__}.{self.obj1.id}"]
+        with open(self.file_path, 'w') as f:
+            json.dump(data, f)
+        storage.reload()
+        objs = storage.all()
+        self.assertEqual(len(objs), 6)
+        self.assertNotIn(f"{type(self.obj1).__name__}.{self.obj1.id}", objs)
 
 
 if __name__ == '__main__':
